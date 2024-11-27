bb3632b8e56bb935396b19da8cb8e25d139686de
jessevanheesch <jessevanheesch2006@gmail.com>
2024-11-27 10:44:31 +0100

    Bow half afgemaakt

---
+++ Conquest RPG/Assets/Scripts/Shooting.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
