17e4e5f45f9f47aa6b9aaaebf7b99b75f95cd965
Ryan Woestenberg <558550@student.fontys.nl>
2024-11-28 10:12:06 +0100

    bug fixes met attacks gefixt. tile pallet met natuur toegevoegd

--- Conquest RPG/Assets/Scripts/the_blob.cs
+++ Conquest RPG/Assets/Scripts/the_blob.cs
@@ -48,10 +48,10 @@ public class the_blob : Enemy
 
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
 
-                changeAnim(temp - transform.position);
+                //changeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
-                anim.SetBool("wakeUp", true);
+                //anim.SetBool("wakeUp", true);
             }
 
         }
@@ -60,34 +60,34 @@ public class the_blob : Enemy
             anim.SetBool("wakeUp", false);
         }
     }
-    private void SetAnimFloat(Vector2 setVector)
-    {
-        anim.SetFloat("moveX", setVector.x);
-        anim.SetFloat("moveY", setVector.y);
+    //private void SetAnimFloat(Vector2 setVector)
+    //{
+    //    anim.SetFloat("moveX", setVector.x);
+    //    anim.SetFloat("moveY", setVector.y);
 
-    }
-    private void changeAnim(Vector2 direction)
-    {
-        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-        {
-            if(direction.x > 0)
-            {
-                SetAnimFloat(Vector2.right);
-            }else if (direction.x < 0) {
-                SetAnimFloat(Vector2.left);
-            }
-        }else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
-        {
-            if (direction.y > 0)
-            {
-                SetAnimFloat(Vector2.up);
-            }
-            else if (direction.y < 0)
-            {
-                SetAnimFloat(Vector2.down);
-            }
-        }
-    }
+    //}
+    //private void changeAnim(Vector2 direction)
+    //{
+    //    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+    //    {
+    //        if(direction.x > 0)
+    //        {
+    //            SetAnimFloat(Vector2.right);
+    //        }else if (direction.x < 0) {
+    //            SetAnimFloat(Vector2.left);
+    //        }
+    //    }else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+    //    {
+    //        if (direction.y > 0)
+    //        {
+    //            SetAnimFloat(Vector2.up);
+    //        }
+    //        else if (direction.y < 0)
+    //        {
+    //            SetAnimFloat(Vector2.down);
+    //        }
+    //    }
+    //}
     private void ChangeState(EnemyState newState)
     {
         if(currentState != newState)
