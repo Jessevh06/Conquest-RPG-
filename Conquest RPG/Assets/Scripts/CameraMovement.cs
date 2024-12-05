28331a3fd39d85214fc18bfb2adc8966e0d1dfa7
Ryan Woestenberg <558550@student.fontys.nl>
2024-12-05 14:42:51 +0100

    camera werkt. en je kunt naar andere scene lopen

--- Conquest RPG/Assets/Scripts/CameraMovement.cs
+++ Conquest RPG/Assets/Scripts/CameraMovement.cs
@@ -11,7 +11,7 @@ public class CamaraMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 
     // Update is called once per frame
@@ -19,7 +19,7 @@ public class CamaraMovement : MonoBehaviour
     {
         if(transform.position != target.position)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, target.position.z);
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
             targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
             targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
